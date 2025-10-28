6b24bc716d51682aa4a98656919bfc48c3ed8d53
aimancode <codebyaiman@gmail.com>
2025-10-28 16:51:50 +0530

    Adding Middleware

--- Understanding-Npm/app.js
+++ Understanding-Npm/app.js
@@ -5,6 +5,15 @@ const express = require("express");
 //creating express application and storing it in app
 const app = express();
 
+//adding the MiddleWare function
+app.use((req, res, next) => {
+  console.log("In The MiddleWare");
+  next(); // Allows the request to continue to the next middleware in line
+});
+
+app.use((req, res, next) => {
+  console.log("In The another MiddleWare!");
+});
 const server = http.createServer(app);
 
 server.listen(3000);
