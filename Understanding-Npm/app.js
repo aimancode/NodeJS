ef9256ddeee0f896bf19f4472114d9e270d2aa28
aimancode <codebyaiman@gmail.com>
2025-10-29 19:31:30 +0530

    4 - Serving Files Statically

--- Understanding-Npm/app.js
+++ Understanding-Npm/app.js
@@ -1,5 +1,6 @@
 const bodyParser = require("body-parser");
 
+const path = require("path");
 const express = require("express");
 
 const adminRoutes = require("./routes/admin");
@@ -9,11 +10,14 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/admin',adminRoutes);
+// servering static folders 
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<p>Page Not Found</p>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
