5a6c440f4d71f65529086788748e10ce1d0cefca
aimancode <codebyaiman@gmail.com>
2025-10-29 23:32:30 +0530

    5 - Pug as Templating Engine

--- 6-Template Engines/routes/shop.js
+++ 6-Template Engines/routes/shop.js
@@ -1,16 +1,15 @@
-const path = require("path");
+// const path = require("path");
 
 const express = require("express");
 
-const rootDir = require("../util/path");
+// const rootDir = require("../util/path");
 const adminData = require("./admin");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  console.log("shop.js",adminData.products);
-
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  const products = adminData.products;
+  res.render("shop", { prods: products, pageTitle:"My Shop", path:"/" });
 });
 
 //getting acces to the products
