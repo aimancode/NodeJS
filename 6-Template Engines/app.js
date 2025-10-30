397471ef9ad0a495066a7b02febd6dd9e95dcd7c
aimancode <codebyaiman@gmail.com>
2025-10-30 13:21:43 +0530

    5 - ejs as templating engine

--- 6-Template Engines/app.js
+++ 6-Template Engines/app.js
@@ -3,22 +3,27 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const expressHbs = require("express-handlebars");
+// const expressHbs = require("express-handlebars");
 
 const app = express();
 
 // app.set("view engine", "pug");
-app.engine("hbs", expressHbs.engine({layoutsDir:"views/layouts/", defaultLayout: 'main-layout.hbs' }));
-app.set("view engine", "hbs");
+
+// app.engine("hbs", expressHbs.engine({layoutsDir:"views/layouts/", defaultLayout: 'main-layout.hbs' }));
+// app.set("view engine", "hbs");
+// app.set("views", "views");
+
+app.set("view engine", "ejs");
 app.set("views", "views");
 
-const adminData = require("./routes/admin");
+
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminData.routes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- 6-Template Engines/routes/shop.js
+++ 6-Template Engines/routes/shop.js
@@ -1,23 +1,10 @@
-// const path = require("path");
 
 const express = require("express");
 
-// const rootDir = require("../util/path");
-const adminData = require("./admin");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  const products = adminData.products;
-  res.render("shop", {
-    prods: products,
-    pageTitle: "My Shop",
-    path: "/",
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true,
-    });
-});
+router.get("/", );
 
 //getting acces to the products
 
