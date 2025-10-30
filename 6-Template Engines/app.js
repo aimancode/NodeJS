f77f9870d16a105e77cf8ebb29e8a0b57115c7c6
aimancode <codebyaiman@gmail.com>
2025-10-30 13:35:44 +0530

    7 - Finishing Controllers

--- 6-Template Engines/app.js
+++ 6-Template Engines/app.js
@@ -16,6 +16,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "views");
 
+const errorController = require("./controllers/erros");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -26,8 +27,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found" });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
